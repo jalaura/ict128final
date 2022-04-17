3a74cba88c17ea977571dda80560ff4a2a0152b4
Meeyaw <jraalu2000@gmail.com>
2022-04-17 09:51:37 -0700

    Update install-latest-chrome.ps1

--- install-latest-chrome.ps1
+++ install-latest-chrome.ps1
@@ -7,16 +7,12 @@
 	PS> ./install-latest-chrome
 #>
 
-function Install-Chrome {
+$StopWatch = [system.diagnostics.stopwatch]::startNew()
+$Path = $env:TEMP;
+$Installer = "chrome_installer.exe"
+Invoke-WebRequest "http://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile $Path\$Installer
+Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait
+Remove-Item $Path\$Installer
 
-	$StopWatch = [system.diagnostics.stopwatch]::startNew()
-
-	$Path = $env:TEMP;
-	$Installer = "chrome_installer.exe"
-	Invoke-WebRequest "http://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile $Path\$Installer
-	Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait
-	Remove-Item $Path\$Installer
-
-	[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
-	"Success! installed Google Chrome in $Elapsed sec"
-}
+[int]$Elapsed = $StopWatch.Elapsed.TotalSeconds
+"Success! installed Google Chrome in $Elapsed sec"
